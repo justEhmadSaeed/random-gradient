699e1576ffb5da8add16a0dd3228ef37e7f8963e
Ehmad <justEhmadSaeed@gmail.com>
2020-02-02 02:04:58 +0500

    IMPROVED JS ES6

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const body = document.querySelector('#gradient');
 const random = document.querySelector('button');
 
 setGradient = () => {
-	body.style.background = 'linear-gradient(to right,' + color1.value + ', ' + color2.value + ' )';
-	h3.textContent = body.style.background;
+	body.style.background = _linearGradient();
+	h3.textContent = `${body.style.background}`;
+};
+_linearGradient = () => {
+	return 'linear-gradient(to right,' + color1.value + ', ' + color2.value + ' )';
 };
 
 color1.addEventListener('input', setGradient);
